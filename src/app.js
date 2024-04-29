97dfc91ccd2840812ffe61575ac990f3cc467a57
AbhijithSPilicode <abhijithspilicode2000@gmail.com>
2024-04-29 16:15:51 +0530

    donetask

--- src/app.js
+++ src/app.js
@@ -47,18 +47,46 @@ function renderPatty() {
 
 function renderCheese() {
   //Trial 1 - Change the visibility of cheese based on state by manipulating the DOM
+  let Cheese = document.querySelector("#cheese");
+  //you can also use getElementById
+  if (state.Cheese) {
+    Cheese.style.display = "inherit";
+  } else {
+    Cheese.style.display = "none";
+  }
 }
 
 function renderTomatoes() {
   //Trial 1 - Change the visibility of Tomatoes based on state by manipulating the DOM
+  let Tomatoes = document.querySelector("#tomato");
+  //you can also use getElementById
+  if (state.Tomatoes) {
+    Tomatoes.style.display = "inherit";
+  } else {
+    Tomatoes.style.display = "none";
+  }
 }
 
 function renderOnions() {
   //Trial 1 - Change the visibility of Onions based on state by manipulating the DOM
+  let Onions = document.querySelector("#onion");
+  //you can also use getElementById
+  if (state.Onions) {
+    Onions.style.display = "inherit";
+  } else {
+    Onions.style.display = "none";
+  }
 }
 
 function renderLettuce() {
   //Trial 1 - Change the visibility of Lettuce based on state by manipulating the DOM
+  let Lettuce = document.querySelector("#lettuce");
+  //you can also use getElementById
+  if (state.Lettuce) {
+    Lettuce.style.display = "inherit";
+  } else {
+    Lettuce.style.display = "none";
+  }
 }
 
 document.querySelector(".btn-patty").onclick = function () {
@@ -67,23 +95,58 @@ document.querySelector(".btn-patty").onclick = function () {
 };
 
 // Trial 2 - Setup event listener for the cheese button
-
+document.querySelector(".btn-cheese").onclick = function () {
+  state.Cheese = !state.Cheese;
+  renderAll();
+};
 
 // Trial 2 - Setup event listener for the tomatoes button
-
+document.querySelector(".btn-tomatoes").onclick = function () {
+  state.Tomatoes = !state.Tomatoes;
+  renderAll();
+};
 
 // Trial 2 - Setup event listener for the onion button
-
+document.querySelector(".btn-onions").onclick = function () {
+  state.Onions = !state.Onions;
+  renderAll();
+};
 
 // Trial 2 - Setup event listener for the lettuce button
-
+document.querySelector(".btn-lettuce").onclick = function () {
+  state.Lettuce = !state.Lettuce;
+  renderAll();
+};
 
 //Challenge 1 - Add/Remove the class active to the buttons based on state
+function renderButtons(){
+  let PattyBtn=document.querySelector(".btn-patty")
+  let CheeseBtn=document.querySelector(".btn-cheese")
+  let tomatoBtn=document.querySelector(".btn-tomato")
+  let onionBtn=document.querySelector(".btn-onion")
+  let lettuceBtn=document.querySelector(".btn-lettuce")
 
+
+  state.Patty?PattyBtn.classList.add("active"):PattyBtn.classList.remove("active")
+  state.Cheese?PattyBtn.classList.add("active"):CheeseBtn.classList.remove("active")
+  state.Tomatoes?PattyBtn.classList.add("active"):tomatoBtn.classList.remove("active")
+  state.Onions?PattyBtn.classList.add("active"):onionBtn.classList.remove("active")
+  state.Lettuce?PattyBtn.classList.add("active"):lettuceBtn.classList.remove("active")
+}
 
 //Challenge 2 - Render only the items selected in the ingredients board based on the state
-
-
+function renderIngredientsBoard(){
+  let Patty=document.querySelector(".items1")
+  let Cheese=document.querySelector(".items2")
+  let Tomatoes=document.querySelector(".items3")
+  let Onions=document.querySelector(".items4")
+  let Lettuce=document.querySelector(".items5")
+  state.Patty?Patty.style.visibility="visible":Patty.style.visibility="hidden"
+  state.Cheese?Cheese.style.visibility="visible":Cheese.style.visibility="hidden"
+  state.Tomatoes?Tomatoes.style.visibility="visible":Tomatoes.style.visibility="hidden"
+  state.Onions?Onions.style.visibility="visible":Onions.style.visibility="hidden"
+  state.Lettuce?Lettuce.style.visibility="visible":Lettuce.style.visibility="hidden"
+}
 //Judgement 1
 //In the p element having price-details as the class, display the calculated
 //price based on ingredients
